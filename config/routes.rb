27b80eaf200956eb08ed0075d94594c5d98fe139
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-03-06 21:25:10 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :artists
   resources :songs
+
+  resources :artists, only: [:show] do
+    # nested resource for posts
+    resources :songs, only: %i[show index]
+  end
+
+  resources :songs, only: %i[index show new create edit update]
+
+  root 'songs#index'
 end
